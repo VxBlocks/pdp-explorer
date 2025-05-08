2ad9623b1b5a8182ff08bc1954db5743b7e30072
MaBarnabas <mawenjie@storswift.com>
2025-05-08 11:11:39 +0800

    fix: :bug: Redirect to the homepage if no history exists.

--- subgraph-client/src/components/go-back.tsx
+++ subgraph-client/src/components/go-back.tsx
@@ -1,4 +1,4 @@
- 
+
 import { Link, useNavigate } from "react-router-dom";
 
 
@@ -7,8 +7,12 @@ export default function GoBackLink() {
     const navigate = useNavigate();
 
     const handleClick = (e: React.MouseEvent) => {
-        e.preventDefault(); // 阻止默认导航
-        navigate(-1); // 回退到上一页 
+        e.preventDefault();
+        if (window.history.length > 1) {
+            navigate(-1);
+        } else {
+            navigate("/");
+        }
     };
     return (<Link to="#" onClick={handleClick} className="text-blue-500 hover:underline">
         ← Go Back
